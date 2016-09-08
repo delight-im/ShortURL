05ea228bec423b8424e4a5f32210671a810a9307
Marco <marco1@live.de>
2016-09-08 09:50:34 +0200

    Add more spacing for clarity

--- PHP/ShortURL.php
+++ PHP/ShortURL.php
@@ -27,19 +27,23 @@ class ShortURL {
 
 	public static function encode($num) {
 		$str = '';
+
 		while ($num > 0) {
 			$str = substr(self::ALPHABET, ($num % self::BASE), 1) . $str;
 			$num = floor($num / self::BASE);
 		}
+
 		return $str;
 	}
 
 	public static function decode($str) {
 		$num = 0;
 		$len = strlen($str);
+
 		for ($i = 0; $i < $len; $i++) {
 			$num = $num * self::BASE + strpos(self::ALPHABET, $str[$i]);
 		}
+
 		return $num;
 	}
 
