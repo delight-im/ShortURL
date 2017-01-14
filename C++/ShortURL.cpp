86aa12d26e598d6964111b67eb31256bfc6a754e
Ujjawal <11.ujjawal@gmail.com>
2017-01-14 22:53:53 +0100

    Add C++ version

---
+++ C++/ShortURL.cpp
@@ -0,0 +1,60 @@
+/*
+ * ShortURL (https://github.com/delight-im/ShortURL)
+ * Copyright (c) delight.im (https://www.delight.im/)
+ * Licensed under the MIT License (https://opensource.org/licenses/MIT)
+ */
+
+/**
+ * ShortURL: Bijective conversion between natural numbers (IDs) and short strings
+ *
+ * ShortURL::encode() takes an ID and turns it into a short string
+ * ShortURL::decode() takes a short string and turns it into an ID
+ *
+ * Features:
+ * + large alphabet (51 chars) and thus very short resulting strings
+ * + proof against offensive words (removed 'a', 'e', 'i', 'o' and 'u')
+ * + unambiguous (removed 'I', 'l', '1', 'O' and '0')
+ *
+ * Example output:
+ * 123456789 <=> pgK8p
+ *
+ * Requirements: C++ 11+
+ */
+
+#include <string>
+#include <stdexcept>
+
+class ShortURL {
+	static const std::string alphabet;
+	static const std::size_t base;
+
+public:
+	static std::string encode(int num) {
+		std::string str;
+
+		while (num > 0) {
+			str = alphabet.at(num % base) + str;
+			num /= base;
+		}
+
+		return str;
+	}
+
+	static int decode(const std::string& str) {
+		int num = 0;
+
+		for (std::string::const_iterator ix = str.cbegin(); ix != str.cend(); ++ix) {
+			std::size_t cIndex = alphabet.find(*ix);
+
+			if (cIndex == std::string::npos)
+				throw std::invalid_argument("Invalid character");
+
+			num = num * base + cIndex;
+		}
+
+		return num;
+	}
+};
+
+const std::string ShortURL::alphabet("23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_");
+const std::size_t ShortURL::base = alphabet.length();
