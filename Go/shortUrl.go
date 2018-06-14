d43fc83493977c156c067aa97db3c82517b035e3
Sandeep Kalra <sandeep.kalra@gmail.com>
2018-06-14 12:31:08 -0500

    Optimized Encode function; rather than using string() to create string object everytime, rely on strings.builder for doing the work on the given object. The builder does not provide a way to insert in front, so we have to reverse the whole thing later.

--- Go/shortUrl.go
+++ Go/shortUrl.go
@@ -24,6 +24,35 @@ const (
 
 type Codec struct{}
 
+// reverseChar Utility to reverse string with only UTF8
+func reverseChars(s string) string {
+	bytes := []byte(s)
+	for i, j := 0, len(bytes)-1; i < j; i, j = i+1, j-1 {
+		bytes[i], bytes[j] = bytes[j], bytes[i]
+	}
+	return string(bytes)
+}
+
+func reverseRunes(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+// Encode: given a generated number, get the URL back
+func (c *Codec) Encode(n int) string {
+	sb := strings.Builder{}
+	for n > 0 {
+		sb.WriteByte(Alphabets[n%Base])
+		n /= Base
+	}
+	// We know that Alphabet set is UTF8, so we will use reverseChars.
+	return reverseChars(sb.String())
+}
+
+/* Deprecated: Faster version is available
 func (c *Codec) Encode(n int) string {
 	var s string
 	for n > 0 {
@@ -32,7 +61,8 @@ func (c *Codec) Encode(n int) string {
 	}
 	return s
 }
-
+*/
+// Decode: given a URL(path), the decoder decodes it to a unique number.
 func (c *Codec) Decode(path string) (int, error) {
 	n := 0
 	for _, c := range path {
@@ -47,6 +77,7 @@ func (c *Codec) Decode(path string) (int, error) {
 	return n, nil
 }
 
+//InitShorteningCodec : Codec for shortening URL and doing the reverse.
 func InitShorteningCodec() *Codec {
 	codec := Codec{}
 	return &codec
