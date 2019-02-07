6e4dc5621f597c01e3855c0ecaf0f8ae87460fc7
Perov.Oleg <perov.oleg2@wildberries.ru>
2019-02-07 20:59:25 +0300

    changed logic and added tests

--- Go/shorturl.go
+++ Go/shorturl.go
@@ -9,59 +9,36 @@
 
 package shorturl
 
-import (
-	"bytes"
-	"math"
-)
+import "strings"
 
-var (
-	alphabet    = [...]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+const (
+	alphabet    = "23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_"
 	alphabetLen = len(alphabet)
-	alphabetMap = makeAlphabetMap()
 )
 
 func Encode(n int) string {
-	var digits []int
+	sb := strings.Builder{}
 	for n > 0 {
-		remainder := n % alphabetLen
-		digits = append(digits, remainder)
+		sb.WriteByte(alphabet[n%alphabetLen])
 		n = n / alphabetLen
 	}
-	reverse(digits)
 
-	var result bytes.Buffer
-	for _, e := range digits {
-		result.WriteString(alphabet[e])
-	}
-	return result.String()
+	return reverse(sb.String())
 }
 
-func Decode(s string) int {
-	var digits []int
-	for _, e := range s {
-		digits = append(digits, alphabetMap[string(e)])
+func Decode(s string) (n int) {
+	for _, r := range s {
+		n = n*alphabetLen + strings.IndexRune(alphabet, r)
 	}
-	reverse(digits)
-
-	var sum int
-	for i, e := range digits {
-		sum += e * int(math.Pow(float64(alphabetLen), float64(i)))
-	}
-	return sum
+	return
 }
 
-func reverse(slice []int) {
-	lastIndex := len(slice) - 1
+func reverse(s string) string {
+	runes := []rune(s)
+	lastIndex := len(s) - 1
 
-	for i := 0; i < len(slice)/2; i++ {
-		slice[i], slice[lastIndex-i] = slice[lastIndex-i], slice[i]
+	for i := 0; i < len(runes)/2; i++ {
+		runes[i], runes[lastIndex-i] = runes[lastIndex-i], runes[i]
 	}
-}
-
-func makeAlphabetMap() map[string]int {
-	alphabetMap := map[string]int{}
-	for i, e := range alphabet {
-		alphabetMap[e] = i
-	}
-	return alphabetMap
+	return string(runes)
 }
